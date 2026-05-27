c344fa94676603241389b013e076a0fb5f39111c
MrGegaz <mrgegaz.dev@gmail.com>
2026-05-27 03:08:53 +0200

    Dodani modeli projekt i zadatak

---
+++ Models/Project.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace taskmanager_mvc.Models;
+
+public class Project
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
+    
+    public string? Description { get; set; }
+    
+    [Required]
+    public DateTime StartDate { get; set; }
+    
+    public DateTime EndDate { get; set; }
+    
+    [Required]
+    public Boolean IsActive { get; set; }
+
+    public ICollection<TaskItem> Tasks { get; set; } = new List<TaskItem>();
+
+}
---
+++ Models/TaskItem.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace taskmanager_mvc.Models;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
+    public string Title { get; set; }
+    
+    public string? Description { get; set; }
+    
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    
+    public DateTime DueDate { get; set; }
+    
+    public enum PriorityLevel
+    {
+        Low,
+        Normal,
+        High
+    }
+    
+    public bool IsCompleted { get; set; }
+    
+    [MaxLength(300)]
+    public string? Notes { get; set; }
+    
+    // Foreign Key
+    public int ProjectId { get; set; }
+    
+    public Project? Project { get; set; }
+}
