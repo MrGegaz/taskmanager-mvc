dd802d0b065030d02c643194960400ec0462c831
MrGegaz <mrgegaz.dev@gmail.com>
2026-05-29 20:26:32 +0200

    Added interface and service DI

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using taskmanager_mvc.Data;
+using taskmanager_mvc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<ITaskService, TaskService>();
 
 var app = builder.Build();
 
@@ -14,6 +18,8 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
+app.UseStaticFiles();
 
 app.UseRouting();
 
