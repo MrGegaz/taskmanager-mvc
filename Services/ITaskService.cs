6128e56d9c8ee1ea0e6d87e95f32bef92cddfb84
MrGegaz <mrgegaz.dev@gmail.com>
2026-05-29 19:03:26 +0200

    Added task interface and service

---
+++ Services/ITaskService.cs
@@ -0,0 +1,14 @@
+﻿using taskmanager_mvc.Models;
+
+namespace taskmanager_mvc.Services;
+
+public interface ITaskService
+{
+    Task<IEnumerable<TaskItem>> GetAllAsync();
+    Task<TaskItem?> GetByIdAsync(int id);
+    Task<IEnumerable<TaskItem>> GetPendingAync();
+    Task<IEnumerable<TaskItem>> SearchByTitleAsync(string query);
+    Task AddAsync(TaskItem task);
+    Task UpdateAsync(TaskItem task);
+    Task DeleteAsync(int id);
+}
---
+++ Services/TaskService.cs
@@ -0,0 +1,49 @@
+﻿using taskmanager_mvc.Data;
+using taskmanager_mvc.Models;
+
+namespace taskmanager_mvc.Services;
+
+public class TaskService : ITaskService
+{
+    protected readonly AppDbContext _context;
+
+    public TaskService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<TaskItem>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<TaskItem?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<TaskItem>> GetPendingAync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<TaskItem>> SearchByTitleAsync(string query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task AddAsync(TaskItem task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateAsync(TaskItem task)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
