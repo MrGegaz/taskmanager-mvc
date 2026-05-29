5cb18054e07792356111ee58d012777110e267f5
MrGegaz <mrgegaz.dev@gmail.com>
2026-05-29 18:57:01 +0200

    Added project interface and service

---
+++ Services/IProjectService.cs
@@ -0,0 +1,12 @@
+﻿using taskmanager_mvc.Models;
+
+namespace taskmanager_mvc.Services;
+
+public interface IProjectService
+{
+    Task<IEnumerable<Project>> GetAllAsync();
+    Task<Project?> GetByIdAsync(int id);
+    Task<Project?> CreateAsync(Project project);
+    Task<Project?> UpdateAsync(Project project);
+    Task<Project> DeleteAsync(int id);
+}
---
+++ Services/ProjectService.cs
@@ -0,0 +1,39 @@
+﻿using taskmanager_mvc.Data;
+using taskmanager_mvc.Models;
+
+namespace taskmanager_mvc.Services;
+
+public class ProjectService : IProjectService
+{
+    private readonly AppDbContext _context;
+
+    public ProjectService(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<Project>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Project?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Project?> CreateAsync(Project project)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Project?> UpdateAsync(Project project)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Project> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
